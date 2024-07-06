27de5c40021ac07c07cc085b8bfe5249800bf5a3
kishanup78910 <kishanup789@gmail.com>
2024-07-06 19:15:50 +0530

    module exports alway return object

--- add.js
+++ add.js
@@ -1,1 +1,11 @@
 console.log("Add modules")
+
+const add = (a,b)=>{
+    return a+b;
+}
+
+const sum =add(2,3);
+
+setTimeout(()=>{
+    console.log(sum)
+},2000)
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 require('./add.js')
+const data = require("./example.js")
 console.log("hi this main")
+console.log(data)
