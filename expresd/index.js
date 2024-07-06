348fc5d7a82579e98eca22cfbd39952539931b2b
kishanup78910 <kishanup789@gmail.com>
2024-07-06 23:47:45 +0530

    express js basic with server

--- expresd/index.js
+++ expresd/index.js
@@ -1,1 +1,36 @@
-console.log("Hello")
+const express = require("express");
+const app=express();
+const path=require('path');
+
+
+app.get("/",(req,res)=>{
+    res.send("<i>helo this is kishan</i>")
+})
+
+
+
+app.get("/contact",(req,res)=>{
+    res.send('<h1>contact page</h1>')
+})
+
+
+app.get("/path",(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+
+
+app.get("/jsonexample",(req,res)=>{
+    res.json([
+        {
+            firsname:"abc",
+            lastName:"def"
+        },
+        {
+            firsname:"kishan ",
+            lastName:"upadhyay"
+        }
+    ])
+})
+app.listen(3000,()=>{
+    console.log("server is running at 3000")
+})
