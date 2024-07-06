e67c02170e2870b4ea486de640d5293401b19443
kishanup78910 <kishanup789@gmail.com>
2024-07-07 00:55:03 +0530

    limit to product query and string query

--- expresd/jsonParamsQuery.js
+++ expresd/jsonParamsQuery.js
@@ -27,14 +27,44 @@ res.send('<h1>Home Page</h1><a href="/api/phone">Phone</a>')
 
 })
 
+//-------------------------------------Query Params-----------------------------------
+//to get a single product using params
 
-app.get('/api/phone',(req,res)=>{
+// app.get('/api/phone/:id',(req,res)=>{
+//     const {id}=req.params;
+//     console.log(req.params);
+//     const singleItem = phone.find((product)=>product.id==Number(id))
+//     res.json(singleItem);
+// })
 
-    const newItem = phone.map((product)=>{
-        const {id,name,image}=product;
-        return {id,name,image}
-    })
-    res.json(newItem);
+
+
+// app.get('/api/phone',(req,res)=>{
+
+//     const newItem = phone.map((product)=>{
+//         const {id,name,image}=product;
+//         return {id,name,image}
+//     })
+//     res.json(newItem);
+// })
+
+//-------------------------------------------------- Query String--------------------------------------------
+app.get('/api/v1/query',(req,res)=>{
+    // console.log(req.query)
+    let sortedPhone = [...phone];
+    const {search,limit}=req.query;
+
+    if(search){
+        sortedPhone=sortedPhone.filter((product)=>{
+            return product.name.startsWith("A");
+        })
+    }
+
+
+    if (limit) {
+        sortedPhone = sortedPhone.slice(0, Number(limit));
+    }
+    res.json(sortedPhone)
 })
 
 app.listen(3000,()=>{
